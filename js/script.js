7fe3c91d69d659c5184756fdca311f8b6865a87f
Kawian <99052760@mydavinci.nl>
2021-04-22 10:34:26 +0200

    bug fixed die mijn statements achteraan liet beginnen ipv gewoon bij de eerste vraag

--- js/script.js
+++ js/script.js
@@ -8,19 +8,22 @@ const contraBtn = document.getElementById("contra");
 var statementOrder = 0;
 var sceneTitle = document.getElementById("sceneTitle");
 var sceneDescription = document.getElementById("sceneDescription");
+var answerQuistjion = [];
 
-startButton.onclick = Start;
+startButton.onclick = start;
 
-//Deze foreach looped door de subjects en voegt de objecten toe
-subjects.forEach((subject) => {
-  subject.myAnswer = "";
+subjects.forEach(subject=> {
+  subject.myAnswer = '';
 });
 
 parties.forEach(party =>{
-    party.points = 0;
-  })
+  party.points = 0;
+})
 
-function Start() {
+/**
+ * De Pagina met de vragen word geladen
+ */
+function start() {
   document.getElementById("homepage").style.display = "none";
   document.getElementById("scenePage").style.display = "block";
 
@@ -28,26 +31,31 @@ function Start() {
   sceneTitle.innerHTML = subjects[0].title;
   sceneDescription.innerHTML = subjects[0].statement;
 }
-
 /**
- * @param answer Is de keuze die je hebt gemaakt (pro, none, contra)
+ * @param answer De keuze die je hebt gemaakt (pro, none, contra)
  */
-proBtn.onclick = setAnswer('pro');
-noneBtn.onclick = setAnswer('none');
-contraBtn.onclick = setAnswer('contra');
+
+ proBtn.onclick = function() {setAnswer('pro')};
+ noneBtn.onclick = function() {setAnswer('none')};
+ contraBtn.onclick = function() {setAnswer('contra')};
+ 
 
 function setAnswer(answer) {
   //De mening word toegevoegd aan answer
   subjects[statementOrder].myAnswer = answer;
   //Nieuwe functie word uitgevoerd
   nextStatement();
-  
 }
 
-noOpinionBtn.onclick = nextStatement;
 
-function nextStatement() {
-  if (statementOrder < subjects.length - 1) {
+/**
+ * Nieuwe stelling word geladen, wanneer je bij de laatste vraag bent word er een functie aangeroepen.
+ */
+
+ noOpinionBtn.onclick = nextStatement;
+
+function nextStatement(){
+  if(statementOrder < subjects.length -1){
     statementOrder++;
     //Nieuwe stelling word geladen
     sceneTitle.innerHTML = subjects[statementOrder].title;
@@ -58,35 +66,40 @@ function nextStatement() {
   else (calculatePoints());
 }
 
+/**
+ * Als de gebruiker op het pijltje terug klikt dan word de vorige vraag geladen 
+ */
+
 previousQuestionBtn.onclick = previousStatement;
 
-function previousStatement() {
-  if (statementOrder !== 0) {
-    statementOrder--;
-    //Oude stelling word geladen
-    sceneTitle.innerHTML = subjects[statementOrder].title;
-    sceneDescription.innerHTML = subjects[statementOrder].statement;
-    showAnswer(subjects[statementOrder].myAnswer);
-  } else {
-    //Als ben je bij de laatste vraag wordt de home pagina weer getoond
-    document.getElementById("scenePage").style.display = "none";
-    document.getElementById("homepage").style.display = "block";
-  }
+  function previousStatement(){
+    console.log(statementOrder);
+    if(statementOrder !== 0){
+        statementOrder--;
+        //Oude stelling word geladen
+        sceneTitle.innerHTML = subjects[statementOrder].title;
+        sceneDescription.innerHTML = subjects[statementOrder].statement;
+        showAnswer(subjects[statementOrder].myAnswer);
+    }else{
+      //Als ben je bij de laatste vraag wordt de home pagina weer getoond
+        document.getElementById("scenePage").style.display = "none";
+        document.getElementById("homepage").style.display = "block";
+    }
 }
 
 /**
  * Als de gebruiker terug gaat naar een eerder ingevulde vraag word het antwoord wat je daar in hebt gevuld getoond
- * @param answer Is de mening die je hebt ingevoerd
+ * @param answer de mening die je hebt ingevoerd
  */
-var answerbtn = document.getElementsByClassName("answerbtn");
 function showAnswer(answer) {
-  for (var f = 0; f < answerbtn.length; f++) {
-    answerbtn[f].style.background = "white";
+  var answerbtn = document.getElementsByClassName('answerbtn');
+  for(var f = 0; f < answerbtn.length; f++) {
+    answerbtn[f].style.background = 'white';
   }
-  if (answer == "") {
-    return false;
+  if(answer == ''){
+    return
   } else {
-    document.getElementById(answer).style.background = "green";
+    document.getElementById(answer).style.background = 'green';
   }
 }
 
@@ -94,19 +107,21 @@ function showAnswer(answer) {
  * Deze functie zorgt er voor dat de punten bij elkaar worden opgeteld
  */
 function calculatePoints() {
-    // Hier loop je door de subjects
-    for(var s = 0; s < subjects.length; s++) {
-        // Hier loop je door de subject partijen
-        for(var p = 0; p < subjects[s].parties.length; p++) {
-            // hier pak je je antwoord en vergelijk je antwoord met welke vraag je hebt beantwoord
-            if(subjects[s].myAnswer == subjects[s].parties[p].position) {
-                //hier worden de punten nij de partij getelt
-                var findParty = parties.find(party => party.name == subjects[s].parties[p].name);
+  //hier loop je door de subjects
+  for(var s=0; s<subjects.length; s++) {
+    //hier loop je door de subject partijen
+    for(var p=0; p<subjects[s].parties.length; p++) {
+      // hier pak je je antwoord en vergelijk je antwoord met welke vraag je hebt beantwoord
+      if(subjects[s].myAnswer == subjects[s].parties[p].position) {
+        //hier worden de punten bij de partij getelt
+        var findParty = parties.find(party => party.name == subjects[s].parties[p].name);
 
-                findParty.points +=1;
-            }
-        }
+        
+          findParty.points +=1;
+      }
     }
+  }
 }
+
 
 
